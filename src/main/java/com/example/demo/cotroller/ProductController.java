3d674b4b703981e9f3363b064e9181aae7ad23bc
magdarosa <magdalena.joanna.rosa@gmail.com>
2024-03-05 19:27:09 +0100

    productList,, CRUD for product, Bootstrap5

--- src/main/java/com/example/demo/cotroller/ProductController.java
+++ src/main/java/com/example/demo/cotroller/ProductController.java
@@ -7,6 +7,7 @@ import java.util.List;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import com.example.demo.model.Product;
@@ -28,6 +29,7 @@ public class ProductController {
     public String showAllProducts(Model model) {
         model.addAttribute("productList", productList);
         model.addAttribute("title", "List of products");
+        model.addAttribute("action", "/saveProduct");
 
         return "/product/products";
     }
@@ -49,6 +51,64 @@ public class ProductController {
             model.addAttribute("product", optionalProduct.get());
         }
         return "/product/product";
+
+    }
+
+    // insert product
+    @PostMapping("/saveProduct")
+    public String saveProduct(Product productForm) {
+        var productExist = productList.stream().map(product -> product.getName())
+                .anyMatch(dbProduct -> productForm.getName().equals(dbProduct));
+
+        if (!productExist) {
+            var nextId = 1;
+            if (!productList.isEmpty()) {
+                var lastId = productList.size() - 1;
+                nextId = productList.get(lastId).getId() + 1;
+            }
+            productForm.setId(nextId);
+            productList.add(productForm);
+
+        }
+
+        return "redirect:/product";
+    }
+
+    // edycja productktu
+    @GetMapping("/editProduct")
+    public String editProduct(@RequestParam Integer productId, Model model) {
+        model.addAttribute("title", "Edit current product");
+        model.addAttribute("action", "/editedProduct?productId=" + productId);
+
+        bindProductToModel(productId, model);
+        return "/product/edit-product";
+    }
+
+    // zapisywanie edytowanego producktu
+    @PostMapping("/editedProduct")
+    public String saveEditedProdutc(@RequestParam Integer productId, Product productForm) {
+        productList = productList.stream().map(
+                product -> {
+                    if (product.getId().equals(productId)) {
+                        productForm.setId(productId);
+                        return productForm;
+                    } else {
+                        return product;
+                    }
+                }).toList();
+        var url = "redirect:/productDetail?productId=" + productId;
+        return url;
+
+    }
+
+    private void bindProductToModel(Integer productId, Model model) {
+        var optionalProduct = productList.stream().filter(dbProduct -> dbProduct.getId().equals(productId)).findFirst();
+        if (optionalProduct.isEmpty()) {
+            // todo:
+        } else {
+            var product = optionalProduct.get();
+            model.addAttribute("product", product);
+        }
 
     }
 
